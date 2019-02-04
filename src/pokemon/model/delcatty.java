6a4c90c76055427a8c68036c938c2a7d17febb7c
squidlywizard <ztdenna@hotmail.com>
2019-02-04 08:44:38 -0700

    all files are made

---
+++ src/pokemon/model/delcatty.java
@@ -0,0 +1,11 @@
+package pokemon.model;
+
+public class delcatty extends skitty
+{
+	public delcatty(int num, String nam)
+	{
+		super(num, nam);
+	
+	}
+
+}
--- src/pokemon/model/skitty.java
+++ src/pokemon/model/skitty.java
@@ -1,6 +1,21 @@
 package pokemon.model;
 
-public class skitty
+public class skitty extends Pokemon implements normalType
 {
+	public skitty(int num, String nam)
+	{
+		super(num,nam);
+	}
+	
+	public void tackle()
+	{
+		
+		
+	}
+	public String perishSong()
+	{
+		
+				return "N";
+	}
 
 }
