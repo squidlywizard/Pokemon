0c315fbc49c4123f23c3e2eb59d1b74731645be1
squidlywizard <ztdenna@hotmail.com>
2019-02-07 08:17:08 -0700

    tryied to fix tests

--- src/pokemon/controller/PokedexController.java
+++ src/pokemon/controller/PokedexController.java
@@ -1,6 +1,9 @@
 package pokemon.controller;
 
+import java.util.function.BooleanSupplier;
+
 import pokemon.view.*;
+import pokemon.model.*;
 
 public class PokedexController
 {
@@ -12,5 +15,25 @@ public class PokedexController
                // System.out.println(simpleBot.processText("test"));
 
 
+	}
+	public Pokemon getPokemonList()
+	{
+		// TODO Auto-generated method stub
+	return null;
+	}
+	public Boolean isInt(String string)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public Boolean isDouble(String string)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public PokedexFrame getFrame()
+	{
+		// TODO Auto-generated method stub
+		return appFrame;
 	}
 }
